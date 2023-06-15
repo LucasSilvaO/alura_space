1e358a54cd5911e07812cefc4f95070c0aa31835
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2023-06-15 17:33:23 -0300

    Adiciona funcionalidades admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links = ("id" , "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable=("publicada",)
+    
+admin.site.register(Fotografia, ListandoFotografias)
